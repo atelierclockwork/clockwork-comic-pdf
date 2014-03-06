730506023c2496b17425012a4367880345a15ff8
Michael Skiba <mike.skiba@me.com>
2014-03-06 00:53:48 -0500

    cleaned up many now obsolete initializer methods, consolidated the attr_writer and attr_accessor declarations into one block

--- lib/clockwork_comic_pdf/book.rb
+++ lib/clockwork_comic_pdf/book.rb
@@ -5,36 +5,23 @@ require_relative 'cover'
 require_relative 'version'
 require_relative 'option_validation'
 require_relative 'book_validation'
-require_relative 'book_init'
 
 module ClockworkComicPDF
   # variable storage for the book class
   class Book
-    include OptionValidation, BookValidation, BookInit
-
-    attr_accessor :toc_font_size
+    include OptionValidation, BookValidation
 
     def initialize(options = {})
-      check_valid(options)
+      load_options options
     end
 
-    def font
-      @font ||= 'Helvetica'
-    end
-    attr_writer :font
+    attr_accessor :toc_font_size, :print_toc, :font, :margin, :print_pagenum,
+                  :page_header, :cover, :base_file_name, :sections, :versions
 
-    def font_size
-      @font_size ||= 12
-    end
+    attr_reader :font_size, :info, :page_size, :offset_from_spine
 
     def font_size=(font_size)
-      @font_size = font_size.to_points unless font_size.nil?
-    end
-
-    attr_accessor :base_file_name
-
-    def info
-      @info ||= {}
+      @font_size = font_size.to_points
     end
 
     def info=(info)
@@ -48,62 +35,20 @@ module ClockworkComicPDF
       @info = info
     end
 
-    def page_size
-      @page_size ||= [{ val: 8.5, type: :in },
-                      { val: 11, type: :in }]
-    end
-
     def page_size=(page_size)
-      page_size = page_size.to_a
-      page_size.each do |dim|
-        page_size[page_size.find_index dim] = dim.to_points
-      end
-      @page_size = page_size
-    end
-
-    def margin
-      @margin ||= { val: 0.25, type: :in }
+      @page_size = page_size.to_points
     end
 
     def margin=(margin)
-      @margin = []
       margin = [margin] unless margin.is_a? Array
       unless margin.count == 4 || margin.count == 1
         fail InvalidValueError 'Margin must contain 1 or 4 values'
       end
-      margin.each_index do |i|
-        @margin[i] = margin[i].to_points
-      end
-      @margin
-    end
-
-    def offset_from_spine
-      @offset_from_spine ||= 0
+      @margin = margin.to_points
     end
 
     def offset_from_spine=(offset_from_spine)
       @offset_from_spine = offset_from_spine.to_points
     end
-
-    def print_toc
-      @print_toc = true if @print_toc.nil?
-      @print_toc
-    end
-    attr_writer :print_toc
-
-    def print_pagenum
-      @print_pagenum = true if @print_toc.nil?
-      @print_pagenum
-    end
-    attr_writer :print_pagenum
-    attr_accessor :page_header
-    attr_accessor :cover
-
-    def versions
-      @versions ||= []
-    end
-    attr_writer :versions
-
-    attr_accessor :sections
   end
 end
