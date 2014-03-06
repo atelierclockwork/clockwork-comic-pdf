add7788a63eb958cfe16d0c380430abc6b2e51c2
Michael Skiba <mike.skiba@me.com>
2014-03-06 00:33:01 -0500

    now parses all options for the book object

--- lib/clockwork_comic_pdf/option_validation.rb
+++ lib/clockwork_comic_pdf/option_validation.rb
@@ -1,38 +1,81 @@
 require 'yaml'
 require_relative 'errors'
+require_relative 'to_points'
 
-# Module-level parameter checker
 module ClockworkComicPDF
-  # provides option validation and requirements vaildaiton
+  # Loads the default options and valid options from the the companion
+  # option_map.yml file, which follows the structure of:
+  # <Class Name>:
+  #   :<Symbol for value name>:
+  #      :type: Class name of Stored Variable
+  #      :default: value for key OR :required: false to signal the key is not
+  #                a required key for this setting during the final validation
+  #
   module OptionValidation
-    def check_valid(options = {})
-      o_list = options_for_class self.class
-      # valid = o_list.reduce([]) { |a, e| a << e[:name] }
-      o_list.each do |option|
-        puts option[:name]
-        send("#{option[:name]}=".to_sym, option[:default]) if option[:default]
-        puts send(option[:name])
+    # Loads the default options for the class
+    def load_defaults
+      o_list.each_pair do |k, v|
+        set_key(k, v[:default]) if  v[:default]
       end
     end
 
-    def options_for_class(o_class)
-      o_map = YAML.load_file(File.join(__dir__, 'option_map.yml'))
-      o_map[o_class.to_s]
+    # Checks the options for valid keys and values and applies them to
+    # the class
+    def load_options(options = {})
+      load_defaults
+      options.each_pair do |k, v|
+        warn = "Unsupported key '#{k}' for '#{self.class}'"
+        fail UndefinedKeyError, warn unless o_list.keys.include? k
+        load_key k, v, o_list[k]
+      end if options
     end
+
+    private
+
+    def o_list
+      o_map = YAML.load_file(File.join(__dir__, 'option_map.yml'))
+      o_map[self.class.to_s]
+    end
+
+    def set_key(k, v)
+      send("#{k}=".to_sym, v)
+    end
+
+    def load_key(k, v, r)
+      return set_key(k, v) if v.class == r[:type]
+      case r[:type]
+      when 'Points' then set_key(k, v.to_points)
+      when 'Boolean' then set_key(k, v == true)
+      else
+        check_ccpdf_classes(k, v, r)
+      end
+    end
+
+    def check_ccpdf_classes(k, v, r)
+      case r[:type]
+      when 'ClockworkComicPDF::PageHeader' then set_key(k, PageHeader.new(v))
+      when 'ClockworkComicPDF::Cover' then set_key(k, Cover.new(v))
+      else
+        fail "Invalid value #{v}" unless v.class.to_s == r[:type]
+        set_key(k, v)
+      end
+    end
+
+    public
 
     def check_options(options, valid_options)
       options.each do |key|
         unless valid_options.include? key
-          fail UndefinedKeyError, "Unsupported key '#{key}' " <<
-                                  "for '#{self.class}'"
+          warn = "Unsupported key '#{key}' for #{self.class}"
+          fail UndefinedKeyError, warn
         end
       end
     end
 
     def check_required(options = {})
       options.each_pair do |key, val|
-        fail UndefinedKeyError, "'#{self.class}' requires " <<
-                                "key '#{key}'" if val.nil?
+        warn = "#{self.class} requires key #{key}"
+        fail UndefinedKeyError, warn if val.nil?
       end
     end
   end
