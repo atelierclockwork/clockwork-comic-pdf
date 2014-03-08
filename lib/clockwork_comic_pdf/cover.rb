17e0e8eb152e83ca7e1cebab046136016a13c06f
Michael Skiba <mike.skiba@me.com>
2014-03-07 22:26:11 -0500

    Implemented option map for the cover class

--- lib/clockwork_comic_pdf/cover.rb
+++ lib/clockwork_comic_pdf/cover.rb
@@ -6,24 +6,14 @@ module ClockworkComicPDF
   # this contains the size, file path, and size of the cover
   class Cover
     include OptionValidation
+    attr_accessor :path, :file
     attr_reader :size
+
     def size=(size)
       unless size.is_a?(Array) && size.count == 2
         throw InvalidValueError.new 'Cover size must contain exactly 2 values'
       end
-      size[0] = size[0].to_points
-      size[1] = size[1].to_points
-      @size = size
-    end
-
-    attr_reader :path
-    def path=(path)
-      @path = path.to_s
-    end
-
-    attr_reader :file
-    def file=(file)
-      @file = file.to_s
+      @size = size.to_points
     end
 
     def validate
@@ -33,10 +23,7 @@ module ClockworkComicPDF
     end
 
     def initialize(options = {})
-      check_options(options.keys, [:size, :path, :file])
-      self.path = options[:path]
-      self.size = options[:size]
-      self.file = options[:file]
+      load_options options
     end
   end
 end
